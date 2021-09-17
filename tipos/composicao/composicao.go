ae63f3c876d587809dae0dbbf9dae525b7bd2d0c
italo-carvalho <italocarvalhoti@hotmail.com>
2021-09-17 18:17:44 -0300

    json, composiçao

---
+++ tipos/composicao/composicao.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+type esportivo interface {
+	ligarTurbo()
+}
+
+type luxuoso interface {
+	fazerBaliza()
+}
+
+type esportivoLuxuoso interface {
+	esportivo
+	luxuoso
+	// pode adiocionar novos métodos
+}
+
+type bmw7 struct{}
+
+func (b bmw7) ligarTurbo() {
+	fmt.Println("Turbo...")
+}
+func (b bmw7) fazerBaliza() {
+	fmt.Println("Baliza...")
+}
+
+func main() {
+	b := bmw7{}
+	b.ligarTurbo()
+	b.fazerBaliza()
+}
